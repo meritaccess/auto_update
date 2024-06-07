2232b0b0cd010bf93bc2a4a6b3714274cec5e5b6
Marek Štec <mar.stec@seznam.cz>
2024-06-07 16:11:02 +0200

    Script tested on real data, TIMEOUT for network added

--- auto_update.sh
+++ auto_update.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-USER="bantj"
+USER="meritaccess"
 APP_DIR="/home/$USER/merit_access_test"
 VERSION_FILE="$APP_DIR/version.txt"
-UPDATE_URL="https://codeload.github.com/meritaccess/update/zip/refs/heads/main"
-VERSION_URL="https://yourserver.com/version.txt"
+REPO="meritaccess/merit_access"
 DOWNLOAD_DIR="/home/$USER/merit_access_update"
-ZIP_NAME="update-main.zip"
-FILE_NAME="update-main"
 LOG_FILE="/home/$USER/logs/update.log"
+NETWORK_TIMEOUT = 20
 
 mkdir -p /home/$USER/logs
 touch $LOG_FILE
@@ -24,43 +22,61 @@ handle_error() {
 
 wait_for_network() {
     log_message "Starting network check."
+    SECONDS=0
     while ! ping -c 1 -W 1 github.com &> /dev/null; do
+        if [ $SECONDS -ge $NETWORK_TIMEOUT ]; then
+            handle_error "Network timeout after $NETWORK_TIMEOUT seconds."
+        fi
         log_message "Waiting for network..."
         sleep 5
     done
     log_message "Network is up."
 }
 
+
 fetch_latest_version() {
-    # curl -s $VERSION_URL
-    echo "4.0.0"
+    echo "$1" | jq -r .tag_name
+}
+
+fetch_asset_url() {
+    echo "$1" | jq -r .zipball_url
 }
 
 update_application() {
+    local asset_url=$1
+    local asset_name="update.zip"
+    echo "$asset_url"
     mkdir -p $DOWNLOAD_DIR || handle_error "Failed to create download directory."
 
-    curl -o $DOWNLOAD_DIR/$ZIP_NAME $UPDATE_URL || handle_error "Failed to download the update."
+    curl -L -o $DOWNLOAD_DIR/$asset_name $asset_url || handle_error "Failed to download the update."
     log_message "Download successful."
 
-    unzip -o $DOWNLOAD_DIR/$ZIP_NAME -d $DOWNLOAD_DIR >> $LOG_FILE 2>&1 || handle_error "Failed to unzip the update package."
+    unzip -o $DOWNLOAD_DIR/$asset_name -d $DOWNLOAD_DIR >> $LOG_FILE 2>&1 || handle_error "Failed to unzip the update package."
     log_message "Unzip successful."
+    
+    unzipped_dir=$(unzip -Z -1 $DOWNLOAD_DIR/$asset_name | head -n 1 | cut -d '/' -f 1)
+    log_message "Unzipped directory: $unzipped_dir"
 
-    rm $DOWNLOAD_DIR/$ZIP_NAME || handle_error "Failed to remove zip file."
-    log_message "Removed zip file."
+    rm -r $APP_DIR/*
+    log_message "Removed old version"
 
-    cp -r $DOWNLOAD_DIR/$FILE_NAME/* $APP_DIR >> $LOG_FILE 2>&1 || handle_error "Failed to copy new files to application directory."
+    cp -r $DOWNLOAD_DIR/$unzipped_dir/* $APP_DIR >> $LOG_FILE 2>&1 || handle_error "Failed to copy new files to application directory."
     log_message "Copied new files to application directory."
 
-    sudo rm -rf $DOWNLOAD_DIR || handle_error "Failed to remove temporary download directory."
+    rm -rf $DOWNLOAD_DIR || handle_error "Failed to remove temporary download directory."
     log_message "Removed temporary download directory."
 
     echo $latest_version > $VERSION_FILE || handle_error "Failed to update version file."
     log_message "Update to version $latest_version completed successfully."
 }
 
+
 wait_for_network
 
-latest_version=$(fetch_latest_version)
+latest_release_info=$(curl -s https://api.github.com/repos/$REPO/releases/latest)
+latest_version=$(fetch_latest_version "$latest_release_info")
+asset_url=$(fetch_asset_url "$latest_release_info")
+
 if [ -f $VERSION_FILE ]; then
     current_version=$(cat $VERSION_FILE)
 else
@@ -69,10 +85,11 @@ fi
 
 log_message "Current version: $current_version"
 log_message "Latest version: $latest_version"
+log_message "Asset URL: $asset_url"
 
 if [ "$latest_version" != "$current_version" ]; then
     log_message "New version available. Updating..."
-    update_application
+    update_application "$asset_url"
 else
     log_message "You already have the latest version."
 fi
